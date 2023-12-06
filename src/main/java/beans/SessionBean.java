100b9ca61418d8ee5866bbaa947a6f92e6cf2751
ehmenzala <ehmenzala@gmail.com>
2023-12-05 21:06:31 -0500

    Add session support

---
+++ src/main/java/beans/SessionBean.java
@@ -0,0 +1,37 @@
+package beans;
+
+import jakarta.faces.context.FacesContext;
+import java.io.IOException;
+
+public class SessionBean {
+
+    private String username;
+    private String passwd;
+    private String allowedUser = "admin";
+    private String allowedPasswd = "admin";
+
+    public void authenticate() throws IOException {
+        if (username.equals(allowedUser) && passwd.equals(allowedPasswd)) {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/zazdentfaces/faces/views/doctors.xhtml");
+        } else {
+            System.out.println("Access denied");
+        }
+    }
+
+    /* Getters and Setters*/
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
